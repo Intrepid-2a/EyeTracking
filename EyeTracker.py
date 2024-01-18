41951e13d1ccb345fc24e9a0a196032ab5bf6a34
Marius <thartbm@gmail.com>
2024-01-18 13:42:36 -0500

    split initialization functions for eye-trackers

--- EyeTracker.py
+++ EyeTracker.py
@@ -2,6 +2,13 @@ import sys
 import warnings
 import numbers
 
+# we will also need to import libraries for both eyetrackers
+# this is for EyeLink devices:
+import pylink
+# this is for LiveTrack devices:
+import LiveTrack
+
+
 class EyeTracker:
 
     def __init__(self, tracker=None, fixationWindow=None, trackEyes=[False, False]):
@@ -15,7 +22,7 @@ class EyeTracker:
             if tracker in ['eyelink', 'livetrack', 'mouse']:
                 self.tracker = tracker
             else:
-                raise Warning("unkown eye-tracker")
+                raise Warning("unkown eye-tracker: %s"%(tracker))
         else:
             raise Warning("tracker must be a string")
 
@@ -39,3 +46,34 @@ class EyeTracker:
                 raise Warning("fixationWindow must be larger than 0")
         else:
             raise Warning("fixationWindow must be a number")
+
+
+    # at the start:
+
+    def initialize(self):
+        if self.tracker in ['eyelink', 'livetrack', 'mouse']:
+            if self.tracker == 'eyelink':
+                self.initialize_eyelink()
+            if self.tracker == 'livetrack':
+                self.initialize_livetrack()
+            if self.tracker == 'mouse':
+                self.initialize_mouse()
+        else:
+            raise Warning("can not initialize tracker: %s"%(self.tracker))
+    
+    def calibrate(self):
+    def savecalibration(self):
+
+    # during trials:
+    def lastsample(self):
+    def testfixation(self):
+    def comment(self):
+
+    # for breaks, and to switch to new files
+    # IIRC on the EyeLink you need to download files when you stopcollecting()?
+
+    def startcollecting(self):
+    def stopcollecting(self):
+
+    # at the end of the experiment:
+    def shutdown(self):
